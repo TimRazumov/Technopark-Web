8f651a1658cbfe22dab069861466496a17f581b6
Tim.Razumov <tima.razumov@gmail.com>
2019-11-02 17:05:33 +0300

    work templates without paginator

---
+++ server_engine/bomonka_forum/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', questions_list, name='questions_list_url'),
+    path('login/', login, name='login_url'),
+    path('signup/', signup, name='signup_url'),
+    path('question/', question, name='question_url'),
+    path('ask/', ask, name='ask_url'),
+]
--- server_engine/bomonka_forum/views.py
+++ server_engine/bomonka_forum/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def questions_list(request):
+    return render(request, 'bomonka_forum/index.html', context={})
+
+def login(request):
+    return render(request, 'bomonka_forum/login.html', context={})
+
+def signup(request):
+    return render(request, 'bomonka_forum/signup.html', context={})
+
+def question(request):
+    return render(request, 'bomonka_forum/question.html', context={})
+
+def ask(request):
+    return render(request, 'bomonka_forum/ask.html', context={})
